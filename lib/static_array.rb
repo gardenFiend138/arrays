f36ace558f137798b7cdcfa44cf6448b8526ea6e
Guy Wassather <ggwass@gmail.com>
2018-01-02 10:12:26 -0800

    Finish static array

---
+++ lib/static_array.rb
@@ -0,0 +1,31 @@
+# This class just dumbs down a regular Array to be statically sized.
+class StaticArray
+  def initialize(length = 0)
+    self.count = length
+    self.store = Array.new(length)
+  end
+
+  # O(1)
+  def [](index)
+    within_range(index) 
+    store[index]
+  end
+
+  # O(1)
+  def []=(index, value)
+    within_range(index)
+    self.store[index] = value
+  end
+  
+  private
+  
+  def within_range(index)
+    if index < 0 || index >= size 
+      raise 'index out of range'
+    end 
+  end
+
+  protected
+  
+  attr_accessor :store, :count
+end
